6ed00b888b3b80b7cb2608a20130226936d90823
alanh90 <ahourmand90@gmail.com>
2024-08-14 17:33:41 -0400

    Working on memory

---
+++ source/bica/new_bica_memory.py
@@ -0,0 +1,93 @@
+class BicaMemory:
+    def __init__(self):
+    def start_sleep_cycle(self):
+    def sleep_cycle(self):
+    def add_memory(self):
+    def update_connections(self):
+
+    def recall_memory(self):
+
+    def filter_by_emotion(self)
+
+    def generate_embedding(self)
+
+    def strengthen_memory(self):
+
+    def consolidate_memories(self):
+
+    def merge_similar_memories(self):
+
+    def merge_emotions(self):
+
+    def generate_merged_content(self):
+
+    def update_importance(self):
+
+    def fade_memories(self):
+
+    def prune_memories(self):
+
+    def activate_memory(self):
+
+    def spread_activation(self):
+
+    def update_active_memories(self):
+
+    def get_active_memories(self):
+
+    def process_conversation_turn(self):
+
+    def initiate_dreaming(self):
+
+    def filter_memories(self):
+
+    def merge_short_term_to_long_term(self):
+
+    def merge_similar_memories_dream(self):
+
+    def simulate_future_situations(self):
+
+    def _generate_positive_scenario(self):
+
+    def _generate_negative_scenario(self):
+
+    def trigger_emotion(self):
+
+    def modulate_emotion_intensity(self):
+
+    def end_dreaming(self):
+
+    def is_dreaming(self):
+
+    def interrupt_dreaming(self):
+
+    def quick_memory_search(self):
+
+    def complex_memory_operation(self):
+
+    def get_memory_by_id(self):
+
+    def get_all_memories(self):
+
+    def get_recent_memories(self):
+
+    def get_important_memories(self):
+
+    def forget_memory(self:
+
+    def adjust_memories(self):
+
+    def generate_memory_summary(self):
+
+    def find_conflicting_memories(self):
+
+    def resolve_memory_conflicts(self):
+
+    def update_memory_connections(self):
+
+    def get_memory_graph(self) -> dict:
+
+    def save_state(self):
+
+    def load_state(self):
+
