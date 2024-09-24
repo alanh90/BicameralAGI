1972e73ce34b9da720dd7b815aed8adb4b6af37d
alanh90 <ahourmand90@gmail.com>
2024-09-23 21:29:41 -0400

    Started on subconscious module

---
+++ source/bica/bica_subconcious.py
@@ -0,0 +1,29 @@
+"""
+Subconscious implementation
+"""
+
+
+class BicaSubconscious:
+    def __init__(self):
+        self.deviation = 0.5
+
+    def subconscious_state(self):
+        pass
+
+    def subconscious_interval(self):
+        pass
+
+    def process_subconscious(self):
+        pass
+
+    def trigger_chaos(self):
+        pass
+
+    def level2(self):
+        pass
+
+    def level3(self):
+        pass
+
+    def level4(self):
+        pass
