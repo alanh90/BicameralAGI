708af06ffa751515d06a1477ac5d6fa81da297e4
alanh90 <ahourmand90@gmail.com>
2024-11-17 19:29:41 -0500

    Uploaded GPU test script for anybody joining in the group

---
+++ bica/help/gpu/gpu_verification.py
@@ -0,0 +1,7 @@
+import torch
+
+print("PyTorch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("CUDA device:", torch.cuda.get_device_name(0))
+    print("CUDA version:", torch.version.cuda)
